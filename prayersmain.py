9efcbca47d865432aa4b8d085b1f20db6e8db8af
JBukhari <56272785+sfc-gh-jbukhari@users.noreply.github.com>
2023-03-30 07:57:33 +0100

    Update prayersmain.py

--- prayersmain.py
+++ prayersmain.py
@@ -2,4 +2,5 @@ import streamlit as st
 import streamlit.components.v1 as components
 # https://github.com/sfc-gh-jbukhari/prayertimes.git
 st.write("true")
-components.html_renderer("<div>true this is prayer time starting now ..... </div>")
+html_render="true - this is prayer time now ..... "
+components.html(html_render, scrolling=True, height=1200)
